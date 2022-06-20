336b692291e0dfe17ec53f43618e7f1352de7eb4
g3th <theinkandpen@yahoo.co.uk>
2022-06-20 02:00:20 +0100

    modified:   dsearch.py 	new file:   funcs.py 	modified:   head.py

--- dsearch.py
+++ dsearch.py
@@ -2,40 +2,51 @@ import requests
 import subprocess
 import shlex
 from head import header
-
-def length(value,i):
-	print(value[i])
-	if len(value[(i)] > v
-	
-	
+from funcs import longest,spaces,takinOurJobs
 
 header()
 
-albums = [];tracklist=[];a=0
-search = input('Search Artist: ')
-
+albums=[]; tracklist=[]; a=0
+		
+search = 'Herbie Hancock' #input('Search Artist: ');header()	
 header()
+
 artist = requests.get('http://api.deezer.com/search/artist/', params={'q':search.replace(" ","+")}).json()
 page = str(artist['data'][0]['tracklist']).replace('limit=50','limit=150')
-
 tracks = requests.get(page).json()
+
 while a < len(tracks['data']):
-	albums.append(tracks['data'][a]['album']['title'])
+	trackRequest = tracks['data'][a]['album']['title']
+	takinOurJobs(trackRequest)
+	
+	if len(trackRequest) > 30:
+		albums.append(trackRequest.replace(trackRequest[28:],'..'))	
+	else:	
+		albums.append(trackRequest)
 	tracklist.append(tracks['data'][a]['album']['tracklist'])
 	a +=1
 	if a == len(tracks['data']):
 		a = 0; break
 
+albums = list(dict.fromkeys(albums))
+tracklist = list(dict.fromkeys(tracklist))
 
+print('Search Returned '+str(len(albums)+1)+' albums, duplicate entries were omitted.\n')
+print(longest(albums))
 
-#while a < len(albums):
-	#print (str(a)+" - "+albums[a]+"   				|    ", end='');print(str(a+1)+" - "+albums[a])
-	#a +=2
+while a != len(albums):	
+	if a < 10:
+		print (' '+str(a+1)+" - "+albums[a]+" "*spaces(albums,a)+"| ",end='')
+		print(''+str(a+2)+" - "+albums[a+1])
+	else:
+		print (str(a+1)+" - "+albums[a]+" "*spaces(albums,a)+"| ",end='')
+		print(str(a+2)+" - "+albums[a+1])
+	a +=2
+
 
 option=input("\nAlbum Number: ")
+albumTracks=requests.get(tracklist[int(option)]).json()
 header()
-albumTracks=requests.get(tracklist[option]).json()
 while a < len(albumTracks['data']):
 	print(str(a)+" - "+albumTracks['data'][a]['title'])
 	a +=1
-	
---
+++ funcs.py
@@ -0,0 +1,28 @@
+
+#longest string in a list ('max()' returns incorrect values)
+
+def longest(value):
+	maxs = '';a=0
+	while a < len(value):
+		if len(value[a]) > len(maxs):
+			maxs = value[a]
+		a +=1
+	return maxs
+	
+#calculate TUI spaces
+	
+def spaces(value, index):
+	nspace = 0
+	if len(value[index]) < len(longest(value)):
+		nspace = len(value[index])+len(longest(value))	
+	else:
+		nspace = len(value[index])
+	return nspace
+
+#string characters are not English (i.e. Japanese, Arabic, Cyrillic...)
+
+def takinOurJobs(string):
+	try:
+		string.encode(encoding='utf-8').decode('ascii')
+	except UnicodeError:
+		string.replace(string[:5],'..')
